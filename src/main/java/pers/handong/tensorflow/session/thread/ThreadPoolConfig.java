4d03dc5d15f876f965000a1d8b1fb021ced1e5fd
handong <455808519@qq.com>
2019-10-21 20:24:04 +0800

    add thread pool config

---
+++ src/main/java/pers/handong/tensorflow/session/thread/ThreadPoolConfig.java
@@ -0,0 +1,22 @@
+package pers.handong.tensorflow.session.thread;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.*;
+
+/**
+ * 线程池配置
+ *
+ * @author handong
+ */
+@Configuration
+public class ThreadPoolConfig {
+
+    @Bean
+    public ExecutorService threadExecutor() {
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("tensorflow-provider-pool-%d").build();
+        return new ThreadPoolExecutor(10, 20, 0, TimeUnit.SECONDS, new LinkedBlockingQueue<>(1), namedThreadFactory);
+    }
+}
