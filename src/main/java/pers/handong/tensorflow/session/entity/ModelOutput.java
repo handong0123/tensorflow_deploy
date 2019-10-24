9d235f242df3555d551422f8c7a264b70e986d0b
handong <455808519@qq.com>
2019-10-24 18:23:08 +0800

    simply getOutput

--- src/main/java/pers/handong/tensorflow/session/entity/ModelOutput.java
+++ src/main/java/pers/handong/tensorflow/session/entity/ModelOutput.java
@@ -1,7 +1,5 @@
 package pers.handong.tensorflow.session.entity;
 
-import org.tensorflow.Tensor;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,8 +12,8 @@ public class ModelOutput {
 
     private Map<String, Object> output = new HashMap<>();
 
-    public Map<String, Object> getOutput() {
-        return output;
+    public Object getOutput(String tensor) {
+        return output.get(tensor);
     }
 
     public void addOutput(String name, Object out) {
